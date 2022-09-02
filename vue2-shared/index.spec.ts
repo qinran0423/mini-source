4c2985082a9afefebd4492dd2e4917220d41cf94
qinran0423 <1156107187@qq.com>
2022-09-02 17:58:38 +0800

    vue2 常用工具

---
+++ vue2-shared/index.spec.ts
@@ -0,0 +1,82 @@
+import { vi } from "vitest"
+import { expect, test } from "vitest"
+import {
+  cached,
+  emptyObject,
+  isArray,
+  isDef,
+  isFalse,
+  isObject,
+  isPlainObject,
+  isPrimitive,
+  isPromise,
+  isTrue,
+  isUndef,
+  toRawType
+} from "."
+
+test("emptyObject", () => {
+  const obj = emptyObject
+  expect(obj).toEqual({})
+})
+
+test("isArray", () => {
+  const arr = []
+  expect(isArray(arr)).toBe(true)
+})
+
+test("isUndef & isDef", () => {
+  let a
+  expect(isUndef(a)).toBe(true)
+  a = 1
+  expect(isDef(a)).toBe(true)
+})
+
+test("isTure", () => {
+  expect(isTrue(true)).toBe(true)
+  expect(isTrue(false)).not.toBe(true)
+})
+
+test("isFalse", () => {
+  expect(isFalse(false)).toBe(true)
+  expect(isFalse(true)).not.toBe(true)
+})
+
+test("isPrimitive", () => {
+  let a = 1
+  expect(isPrimitive(a)).toBe(true)
+  let b = {}
+  expect(isPrimitive(b)).not.toBe(true)
+})
+
+test("isObject", () => {
+  expect(isObject([])).toBe(true)
+  expect(isObject(null)).not.toBe(true)
+  expect(isObject({})).toBe(true)
+})
+
+test("toRawType", () => {
+  expect(toRawType("")).toBe("String")
+  expect(toRawType()).toBe("Undefined")
+})
+
+test("isPlainObject", () => {
+  expect(isPlainObject([])).toBe(false)
+  expect(isPlainObject({})).toBe(true)
+})
+
+test("isPromise", () => {
+  const p1 = new Promise(function (resolve, reject) {
+    resolve("mick")
+  })
+  expect(isPromise(p1)).toBe(true)
+})
+
+test("cached", () => {
+  const fnMock = vi.fn((str) => str)
+  const getName = cached(fnMock)
+  const name = getName("name")
+  expect(name).toBe("name")
+  expect(fnMock).toHaveBeenCalled()
+  expect(fnMock).toHaveBeenCalledTimes(1)
+})
---
+++ vue2-shared/index.ts
@@ -0,0 +1,62 @@
+export const emptyObject = Object.freeze({})
+
+export const isArray = Array.isArray
+
+export function isUndef(v) {
+  return v === undefined || v === null
+}
+export function isDef(v) {
+  return v !== undefined && v !== null
+}
+
+export function isTrue(v) {
+  return v === true
+}
+
+export function isFalse(v) {
+  return v === false
+}
+
+export function isPrimitive(value) {
+  return (
+    typeof value === "string" ||
+    typeof value === "number" ||
+    // $flow-disable-line
+    typeof value === "symbol" ||
+    typeof value === "boolean"
+  )
+}
+
+export function isObject(obj) {
+  return obj !== null && typeof obj === "object"
+}
+
+const _toString = Object.prototype.toString
+export function toRawType(value?) {
+  return _toString.call(value).slice(8, -1)
+}
+
+export function isPlainObject(obj) {
+  return _toString.call(obj) === "[object Object]"
+}
+
+export function isPromise(val) {
+  return (
+    isDef(val) &&
+    typeof val.then === "function" &&
+    typeof val.catch === "function"
+  )
+}
+
+export function cached(fn) {
+  const cache = Object.create(null)
+  return function cachedFn(str) {
+    var hit = cache[str]
+    return hit || (cache[str] = fn(str))
+  }
+}
+
+// const camelizeRE = /-(\w)/g
+// export const camelize = cached((str) => {
+//   return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ""))
+// })
